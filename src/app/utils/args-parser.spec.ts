5ef33711fa6aef3fcabe50cf8011cc381180a30e
Nikita Agupov <nktrsk@gmail.com>
2019-04-11 11:04:15 +0300

    microfix common: update parser util

--- src/app/utils/args-parser.spec.ts
+++ src/app/utils/args-parser.spec.ts
@@ -3,17 +3,17 @@ import { ArgsParser } from './args-parser';
 describe('ArgsParser', () => {
 
     it('should correct parse empty args', () => {
-        expect(ArgsParser.get([])).toEqual(new Map());
+        expect(ArgsParser.get([])).toEqual(new Map([['_', []]]));
     });
 
-    it('should filter args without key', () => {
-        expect(ArgsParser.get(['cat', 'dog'])).toEqual(new Map());
-        expect(ArgsParser.get(['cat', '-animal', 'dog'])).toEqual(new Map([['animal', ['dog']]]));
+    it('should parse args without key', () => {
+        expect(ArgsParser.get(['cat', 'dog'])).toEqual(new Map([['_', ['cat', 'dog']]]));
+        expect(ArgsParser.get(['cat', '-animal', 'dog'])).toEqual(new Map([['_', ['cat']], ['animal', ['dog']]]));
     });
 
     it('should parse keys starting with a hyphen', () => {
-        expect(ArgsParser.get(['-animal', 'dog'])).toEqual(new Map([['animal', ['dog']]]));
-        expect(ArgsParser.get(['--animal', 'dog'])).toEqual(new Map([['animal', ['dog']]]));
-        expect(ArgsParser.get(['ani-mal', 'dog'])).toEqual(new Map());
+        expect(ArgsParser.get(['-animal', 'dog'])).toEqual(new Map([['_', []], ['animal', ['dog']]]));
+        expect(ArgsParser.get(['--animal', 'dog'])).toEqual(new Map([['_', []], ['animal', ['dog']]]));
+        expect(ArgsParser.get(['ani-mal', 'dog'])).toEqual(new Map([['_', ['ani-mal', 'dog']]]));
     });
 });
